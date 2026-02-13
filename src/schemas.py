7f3811c24cbfe11124f1e0491b6a9d72b848f3b1
AlexGreasley1 <aejg201@exeter.ac.uk>
2026-02-13 12:39:36 +0000

    Added Comments to schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 
 class SimulationRequest(BaseModel):
+    """
+    Defines the input structure for the simulation request.
+    """
     price: float
     discount: float
     lead_time: float
